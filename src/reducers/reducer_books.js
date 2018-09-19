fa6fecf45326c488d543f36671f2a73ca80e1ebe
MonikaP <piechatzek@hotmail.com>
2018-09-19 17:34:31 -0600

    book list added

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
 	return [
-		{ title: 'Twilight' },
-		{ title: 'New Moon' },
-		{ title: 'Eclipse' },
-		{ title: 'Breaking Dawn' }
+		{ title: 'Twilight', pages: 10 },
+		{ title: 'New Moon', pages: 20 },
+		{ title: 'Eclipse', pages: 30 },
+		{ title: 'Breaking Dawn', pages: 40 }
 	]
 }
